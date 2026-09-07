31aef85c896bc1972c4370da237a68281f1fa8c0
alex-melia <alexmelia41@gmail.com>
2026-09-08 01:50:48 +0900

    fix lint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono, Ubuntu, Ubuntu_Mono } from "next/font/google"
-// @ts-expect-error The stylesheet is processed by Next.js at build time.
 import "./globals.css"
 import Navbar from "@/components/layout/navbar"
 import { ThemeProvider } from "next-themes"
